7277458f8b48d0809044d72e41a6b71a95be57a0
dillinger, austin <atdillinger92@gmail.com>
2018-07-10 18:54:26 -0500

    intial commit

---
+++ add-function/run.py
@@ -0,0 +1,7 @@
+import os
+import json
+
+postreqdata =json.loads(open(os.environ['req']).read())
+response = open(os.environ['res'], 'w')
+response.write("Hello"+postreqdata['name'])
+response.close()
