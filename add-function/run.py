ac9ff35d297f7baf7680b4de7c65cbd3ac0760be
Joey Hage <joecool520@gmail.com>
2018-07-11 21:03:20 -0500

    Responding with entire postreqdata

--- add-function/run.py
+++ add-function/run.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-postreqdata =json.loads(open(os.environ['req']).read())
+postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
 response.write("Hello"+postreqdata['name'])
 response.close()
--- experimentation/run.py
+++ experimentation/run.py
@@ -1,5 +1,7 @@
 import os
+import json
 
+postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
-response.write(os.environ['req'])
+response.write(postreqdata)
 response.close()
